f3dc81681a344f933f6fff8ebe0effba20f0c0a6
manish mandal <76776194mm@gmail.com>
2023-06-12 20:31:55 +0530

    initialized sign in component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
 	return <h2>This is a Shop</h2>;
 };
+
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigation />}>
-				<Route index element={<Home />} />
+				<Route index={true} element={<Home />} />
 				<Route path='shop' element={<Shop />} />
+				<Route path='sign-In' element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
